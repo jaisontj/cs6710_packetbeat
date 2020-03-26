e6849bbc74457c1a4b28bb48005a6c2c092a3a0d
cmullen1109 <45216542+cmullen1109@users.noreply.github.com>
2020-03-26 13:08:05 -0400

    add comments

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+echo "[+] Run me as root!"
+echo "Setting up Docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker pi
+echo "Setting up Docker Compose"
 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
