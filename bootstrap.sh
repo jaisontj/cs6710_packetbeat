646af766f29080669cf64772fb009b7b2de64282
Jaison Titus <jaisontj92@gmail.com>
2020-03-31 10:59:06 -0400

    Fixed typo in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,4 @@ usermod -aG docker pi
 systemctl start docker.service
 echo "Setting up Docker Compose"
 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compoe
+chmod +x /usr/local/bin/docker-compose
